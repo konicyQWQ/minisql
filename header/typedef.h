3023d98e21a356239ea73806870cdfaad1840b44
cnsky1103 <cnsky1103@gmail.com>
2020-06-05 15:46:00 +0800

    Update typedef.h
    添加了Tuple和WhereQuery的定义

--- header/typedef.h
+++ header/typedef.h
@@ -1,14 +1,70 @@
-#ifndef TYPEDEF_H
-#define TYPEDEF_H
+//
+// Created by sky on 2020/6/3.
+//
+
+#ifndef MINISQL_TYPEDEF_H
+#define MINISQL_TYPEDEF_H
 
 #include <string>
+#include <utility>
 #include <vector>
+
+class Data
+{
+public:
+    short type;//0:int;1:float;otherwise vchar
+};
+
+class iData : public Data
+{
+public:
+    int value;
+
+    explicit iData(int x) : Data(), value(x)
+    {
+        type = 0;
+    }
+};
+
+class fData : public Data
+{
+public:
+    float value;
+
+    explicit fData(float x) : Data(), value(x)
+    {
+        type = 1;
+    }
+};
+
+class sData : public Data
+{
+public:
+    std::string value;
+
+    explicit sData(std::string x) : Data(), value(std::move(x))
+    {
+        type = x.size();
+    }
+};
+
+
+class Tuple
+{
+private:
+    std::vector<Data *> data;
+public:
+    explicit Tuple(std::vector<Data *> d) : data(std::move(d))
+    {
+
+    }
+};
 
 typedef unsigned char byte;
 
-
 /* 属性 */
-struct Attribute {
+struct Attribute
+{
     std::string name;   // 属性名(不超过80个字符)
     byte type, length;  // 类型：type=0 表示 int，type=1 表示 float，type=2 表示 char（length为char的长度）
     bool isUnique;      // 是否是唯一的
@@ -16,13 +72,15 @@ struct Attribute {
 };
 
 /* 索引 */
-struct Index {
+struct Index
+{
     std::string name;  // 索引名(不超过80个字符)
     byte indexNum;     // 索引在第几个属性上，Index和它所在的表有关，不能与表分开使用
 };
 
 /* 表 */
-class Table {
+class Table
+{
 public:
     std::string name;           // 表名
     byte attrCnt;               // 属性数量
@@ -30,7 +88,18 @@ public:
     byte primary;               // 主键在第几个属性上, -1也就是255表示没有主键
     Index index[32];            // 最多32个索引，从0开始
     Attribute attr[32];         // 最多32个属性，从0开始
-    //std::vector<Tuple *> tuple; // 所有的元组
+    std::vector<Tuple *> tuple; // 所有的元组
 };
 
-#endif
+typedef enum
+{
+    e, ne, gt, lt, ge, le
+} COMPARE;
+struct WhereQuery
+{
+    std::string col;
+    COMPARE op;
+    byte type;
+    std::string value;//这里我们把一切
+};
+#endif //MINISQL_TYPEDEF_H
